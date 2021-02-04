ef379f9a304a911e85f63ca732e932c6bcdfb5c4
dchalbyshev <dchalbyshev@gmail.com>
2021-02-04 22:30:22 +0300

    best Solution

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,12 +3,15 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class UserMealsUtil {
@@ -24,29 +27,44 @@ public class UserMealsUtil {
         );
 
         List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+
+
         mealsTo.forEach(System.out::println);
 
 //        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+       meals.forEach(m-> System.out.println(m));
+
+      /// метод reference  освежить
+
         // TODO return filtered list with excess. Implement by cycles
         return null;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-       List<UserMealWithExcess> list = new ArrayList<>();
-       // через поток сортирую еду которая превышает количесство установленных каллорий приема пищи для данного временного промежутка
-        meals.stream().filter(p-> TimeUtil.isBetweenHalfOpen(p.getDateTime().toLocalTime(),startTime,endTime)).
-                filter(p-> p.getCalories()>caloriesPerDay).forEach(p->list.add(new UserMealWithExcess(p.getDateTime(),p.getDescription(),
-                p.getCalories(),true)));
+       Map<LocalDate,Integer> calloriesSumByDate =  meals.stream().collect(Collectors.groupingBy(um->um.getDateTime().toLocalDate(),
+               Collectors.summingInt(UserMeal::getCalories)));// получаем сумму калоррий по дням
+       return meals.stream().filter(um->TimeUtil.isBetweenHalfOpen(um.getDateTime().toLocalTime(),
+                startTime,endTime)).map(um->new UserMealWithExcess(um.getDateTime(),
+                um.getDescription(),um.getCalories(),calloriesSumByDate.get(um.getDateTime().toLocalDate())>caloriesPerDay))
+                .collect(Collectors.toList());
 
+       // collect() - терминальная опреация преобр. stream
+        // Map <localDate,List<UserMeal>> list = meals.stream().collect(Collectors.groupingBy(um->um.getDateTime().toLocalDate()))
+         // сформировали мапу  ключ дата, значение - список еды в данную дату
+        // Collectors.summingInt(um->um.getCalories())));  вторым считем сумму
+        // UserMeal::getCalories)) -  идея предложила - мы поменяли на метод  reference
 
-
+        // map() перобарзование коллекции
+       //  .collect() - надо завершить операцию преобразовать во что то
         // TODO Implement by streams
-        return list;
+
+       //  Collectors.toMap() применяется вместо Collectors.groupingBy при более слложной логике
+
     }
 
-    //  не понял наличие boolean переменной у класаа еда ( с превышением(априори она true)
+    //  не совсем понял наличие boolean переменной у класа еда ( с превышением(априори она true))
     // почему бы не сделать bean - прием пищи(callories,data,discription) и день в котром бы хранился список приемов пищи?
 }
