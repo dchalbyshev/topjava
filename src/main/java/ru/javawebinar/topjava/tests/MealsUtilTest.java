6fb5a22b2273b157fc737dd57338423c23409f01
dchalbyshev <dchalbyshev@gmail.com>
2021-02-17 12:23:02 +0300

    2.1patch

---
+++ src/main/java/ru/javawebinar/topjava/tests/MealsUtilTest.java
@@ -0,0 +1,26 @@
+package ru.javawebinar.topjava.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MealsUtilTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void filteredByStreams() {
+    }
+}
---
+++ src/main/webapp/ru/javawebinar/topjava/web/MealServletTest.java
@@ -0,0 +1,7 @@
+package ru.javawebinar.topjava.web;
+
+import static org.junit.Assert.*;
+
+public class MealServletTest {
+
+}
